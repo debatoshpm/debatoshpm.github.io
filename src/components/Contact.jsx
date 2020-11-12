33edc09b4875c0fd05c43253fa03d63c6b36e85b
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2020-11-13 03:27:34 +0530

    minor size fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,10 +15,10 @@ function Contact() {
         If you like my work and have some cool project to work on, just send me
         direct message or contact me through social sites listed below.
       </div>
-      <div className="text-center p-5">
-        <i class="fab fa-linkedin fa-7x mr-5 maincontent icn"></i>
-        <i class="fab fa-github-square fa-7x mr-5 maincontent icn"></i>
-        <i class="fas fa-envelope-square fa-7x maincontent icn"></i>
+      <div className="text-center p-5 container">
+        <i class="fab fa-linkedin fa-7x ml-5 mr-5 maincontent icn"></i>
+        <i class="fab fa-github-square fa-7x ml-5 mr-5 maincontent icn"></i>
+        <i class="fas fa-envelope-square fa-7x ml-5 mr-5 maincontent icn"></i>
       </div>
     </React.Fragment>
   );
