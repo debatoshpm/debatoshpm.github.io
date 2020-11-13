9a1b1f71dfa3ee6b2909af42c862ba229aa18bda
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2020-11-13 15:45:11 +0530

    skill changes added

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -10,23 +10,41 @@ class Skill extends Component {
       <React.Fragment>
         <Nav />
         <Navbar />
-        <div className="m-5">
+        <div className="mt-5">
           <div className="abouthead text-center">Skills</div>
           <div className="aboutcontent text-center">...</div>
         </div>
         <div className="container">
           <div className="row">
-            <div className="col-md-4">
-              <Meter data={{ value: 10 }} />
-              <p className="text-center restfont mt-2 mb-2">HTML</p>
+            <div className="col-md-4 mt-4">
+              <Meter data={{ value: 70 }} />
+              <p className="text-center restfont mt-2 mb-2">C/C++</p>
             </div>
-            <div className="col-md-4">
-              <Meter data={{ value: 10 }} />
-              <p className="text-center restfont mt-2 mb-2">HTML</p>
+            <div className="col-md-4 mt-4">
+              <Meter data={{ value: 90 }} />
+              <p className="text-center restfont mt-2 mb-2">
+                HTML CSS SASS JS PHP MySQL
+              </p>
             </div>
-            <div className="col-md-4">
-              <Meter data={{ value: 10 }} />
-              <p className="text-center restfont mt-2 mb-2">HTML</p>
+            <div className="col-md-4 mt-4">
+              <Meter data={{ value: 80 }} />
+              <p className="text-center restfont mt-2 mb-2">MERN</p>
+            </div>
+            <div className="col-md-4 mt-4">
+              <Meter data={{ value: 80 }} />
+              <p className="text-center restfont mt-2 mb-2">
+                Bootstrap MaterialUI
+              </p>
+            </div>
+            <div className="col-md-4 mt-4">
+              <Meter data={{ value: 80 }} />
+              <p className="text-center restfont mt-2 mb-2">Python</p>
+            </div>
+            <div className="col-md-4 mt-4">
+              <Meter data={{ value: 60 }} />
+              <p className="text-center restfont mt-2 mb-2">
+                SciPy ScikitLearn OpenCV
+              </p>
             </div>
           </div>
         </div>
